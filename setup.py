d2c219d6b5c557db3eede93f1eac0ecea38b982d
Tim Paine <t.paine154@gmail.com>
2018-06-15 10:26:17 -0400

    up version

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='nannotate',
-    version='0.0.1',
+    version='0.0.2',
     description='Automated dataset annotation',
     long_description=long_description,
     url='https://github.com/timkpaine/nannotate',
-    download_url='https://github.com/timkpaine/nannotate/archive/v0.0.1.tar.gz',
+    download_url='https://github.com/timkpaine/nannotate/archive/v0.0.2.tar.gz',
     author='Tim Paine',
     author_email='timothy.k.paine@gmail.com',
     license='GPL',
