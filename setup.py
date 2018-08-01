5f4b1c1322c7a19bfccabdc56132ec21d26d146b
Tim Paine <t.paine154@gmail.com>
2018-08-01 12:08:03 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     download_url='https://github.com/timkpaine/nannotate/archive/v0.0.4.tar.gz',
     author='Tim Paine',
     author_email='timothy.k.paine@gmail.com',
-    license='GPL',
+    license='Apache 2.0',
 
     classifiers=[
         'Development Status :: 3 - Alpha',
